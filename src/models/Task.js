c337cd1e700ccb7cea43ef70aa93cee4d18ba0ba
jamesosei-hub <jamesoseiamaniampongagyemang@gmail.com>
2026-03-07 02:58:08 +0000

    Completed Task Manager REST API assignment

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,17 +1,20 @@
-import mongoose from "mongoose";
-
-// Create Task schema
-// Fields:
-// - title (String, required)
-// - description (String)
-// - completed (Boolean, default false)
-// - owner (ObjectId, ref "User", required)
-// - createdAt (default Date.now)
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  
+  title: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
+<<<<<<< HEAD
 const Task = mongoose.model("Task", taskSchema);
 
 export default tasks;
+=======
+module.exports = mongoose.model("Task", taskSchema);
+>>>>>>> 480eebc55ba9edc1983733dcd8a69a3b1ee84026
